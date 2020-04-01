75f03df41200fcb6c86c1fd5f005cc18243f2fa7
JonVeD <31772814+JonVeD@users.noreply.github.com>
2020-04-02 01:23:40 +0200

    added lorem to help

--- quasar-todo/src/pages/Help.vue
+++ quasar-todo/src/pages/Help.vue
@@ -3,12 +3,11 @@
     <h5 class="q-mt-none">Help</h5>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit eligendi, ab corrupti itaque sunt explicabo recusandae architecto perspiciatis consectetur non vel aperiam, temporibus sed, modi inventore a! Accusantium omnis ducimus ex labore nobis. Cumque, veniam. Corporis labore ducimus obcaecati sed, quam vero eos corrupti molestiae. Obcaecati nesciunt debitis animi dolorem? Non magni quasi dicta dolor in, velit ab ex. Neque voluptate hic, atque, perferendis eos amet harum cupiditate non eum incidunt rerum saepe distinctio pariatur ex unde odio quis illo? Labore eius, voluptas laboriosam eaque nostrum provident est ratione maxime necessitatibus libero totam rerum cum mollitia architecto ipsum delectus quaerat.</p>
   </q-page>
 </template>
 
 <script>
 export default {
 }
-
-document.addEventListener('DOMContentLoaded', getStatus);
 </script>

577fbd30b7840e7c93a35edd7e6560cfe3ead69d
JonVeD <31772814+JonVeD@users.noreply.github.com>
2020-04-04 01:59:56 +0200

    fixed lint

--- quasar-todo/src/layouts/MainLayout.vue
+++ quasar-todo/src/layouts/MainLayout.vue
@@ -1,112 +1,121 @@
 <template>
-<q-layout view="lHh Lpr lFf">
-  <q-header>
-    <q-toolbar>
-      <q-btn flat dense round icon="menu" aria-label="Menu" @click="leftDrawerOpen = !leftDrawerOpen" />
-    </q-toolbar>
-    <div class="q-px-lg q-pt-xl q-mb-md">
-      <div class="text-h3">Todo</div>
-      <div class="text-subtitle1">{{ todaysDate }}</div>
-    </div>
-    <q-img src="statics/chalk.jpg" class="header-image absolute-top" />
-
-  </q-header>
-
-  <q-drawer v-model="leftDrawerOpen" show-if-above :width="250" :breakpoint="600">
-    <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
-      <q-list padding>
-
-        <q-item to="/" exact clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="list" />
-          </q-item-section>
-
-          <q-item-section>
-            Todo
-          </q-item-section>
-        </q-item>
-
-        <q-item to="/help" exact clickable v-ripple>
-          <q-item-section avatar>
-            <q-icon name="help" />
-          </q-item-section>
-
-          <q-item-section>
-            Help
-          </q-item-section>
-        </q-item>
-
-      </q-list>
-    </q-scroll-area>
-
-    <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 192px; filter:grayscale(100%);">
-      <div class="absolute-bottom bg-transparent">
-        <div class="text-weight-bold">Razvan Stoenescu</div>
-        <div>@rstoenescu</div>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo App</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
       </div>
-    </q-img>
-  </q-drawer>
+      <q-img src="statics/chalk.jpg" class="header-image absolute-top" />
+    </q-header>
 
-  <q-page-container>
-    <keep-alive>
-      <router-view />
-    </keep-alive>
-  </q-page-container>
-</q-layout>
+    <q-drawer v-model="leftDrawerOpen" show-if-above :width="250" :breakpoint="600">
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section>Todo</q-item-section>
+          </q-item>
+
+          <q-item to="/help" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section>Help</q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="https://cdn.quasar.dev/img/material.png"
+        style="height: 192px; filter: grayscale(100%);"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <div class="text-weight-bold">Razvan Stoenescu</div>
+          <div>@rstoenescu</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import {
-  date
-} from 'quasar'
+import { date } from 'quasar'
 
 export default {
   name: 'MainLayout',
 
-  data () {
+  data() {
     return {
       leftDrawerOpen: false,
-      essentialLinks: [{
-        title: 'Docs',
-        caption: 'quasar.dev',
-        icon: 'school',
-        link: 'https://quasar.dev'
-      },
-      {
-        title: 'Github',
-        caption: 'github.com/quasarframework',
-        icon: 'code',
-        link: 'https://github.com/quasarframework'
-      },
-      {
-        title: 'Discord Chat Channel',
-        caption: 'chat.quasar.dev',
-        icon: 'chat',
-        link: 'https://chat.quasar.dev'
-      },
-      {
-        title: 'Forum',
-        caption: 'forum.quasar.dev',
-        icon: 'record_voice_over',
-        link: 'https://forum.quasar.dev'
-      },
-      {
-        title: 'Twitter',
-        caption: '@quasarframework',
-        icon: 'rss_feed',
-        link: 'https://twitter.quasar.dev'
-      },
-      {
-        title: 'Facebook',
-        caption: '@QuasarFramework',
-        icon: 'public',
-        link: 'https://facebook.quasar.dev'
-      }
+      essentialLinks: [
+        {
+          title: 'Docs',
+          caption: 'quasar.dev',
+          icon: 'school',
+          link: 'https://quasar.dev'
+        },
+        {
+          title: 'Github',
+          caption: 'github.com/quasarframework',
+          icon: 'code',
+          link: 'https://github.com/quasarframework'
+        },
+        {
+          title: 'Discord Chat Channel',
+          caption: 'chat.quasar.dev',
+          icon: 'chat',
+          link: 'https://chat.quasar.dev'
+        },
+        {
+          title: 'Forum',
+          caption: 'forum.quasar.dev',
+          icon: 'record_voice_over',
+          link: 'https://forum.quasar.dev'
+        },
+        {
+          title: 'Twitter',
+          caption: '@quasarframework',
+          icon: 'rss_feed',
+          link: 'https://twitter.quasar.dev'
+        },
+        {
+          title: 'Facebook',
+          caption: '@QuasarFramework',
+          icon: 'public',
+          link: 'https://facebook.quasar.dev'
+        }
       ]
     }
   },
   computed: {
-    todaysDate () {
+    todaysDate() {
       return date.formatDate(Date.now(), 'dddd D MMMM')
     }
   }
@@ -115,9 +124,9 @@ export default {
 
 <style lang="scss">
 .header-image {
-    height: 100%;
-    z-index: -1;
-    opacity: 0.2;
-    filter: grayscale(100%);
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
 }
 </style>
--- quasar-todo/src/pages/Help.vue
+++ quasar-todo/src/pages/Help.vue
@@ -1,13 +1,48 @@
 <template>
   <q-page class="q-pa-lg">
     <h5 class="q-mt-none">Help</h5>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit eligendi, ab corrupti itaque sunt explicabo recusandae architecto perspiciatis consectetur non vel aperiam, temporibus sed, modi inventore a! Accusantium omnis ducimus ex labore nobis. Cumque, veniam. Corporis labore ducimus obcaecati sed, quam vero eos corrupti molestiae. Obcaecati nesciunt debitis animi dolorem? Non magni quasi dicta dolor in, velit ab ex. Neque voluptate hic, atque, perferendis eos amet harum cupiditate non eum incidunt rerum saepe distinctio pariatur ex unde odio quis illo? Labore eius, voluptas laboriosam eaque nostrum provident est ratione maxime necessitatibus libero totam rerum cum mollitia architecto ipsum delectus quaerat.</p>
+    <h5 class="q-item__section">22</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit eligendi,
+      ab corrupti itaque sunt explicabo recusandae architecto perspiciatis
+      consectetur non vel aperiam, temporibus sed, modi inventore a! Accusantium
+      omnis ducimus ex labore nobis. Cumque, veniam. Corporis labore ducimus
+      obcaecati sed, quam vero eos corrupti molestiae. Obcaecati nesciunt
+      debitis animi dolorem? Non magni quasi dicta dolor in, velit ab ex. Neque
+      voluptate hic, atque, perferendis eos amet harum cupiditate non eum
+      incidunt rerum saepe distinctio pariatur ex unde odio quis illo? Labore
+      eius, voluptas laboriosam eaque nostrum provident est ratione maxime
+      necessitatibus libero totam rerum cum mollitia architecto ipsum delectus
+      quaerat.
+    </p>
+    <ul class="list">
+      <li class="item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, illum!</li>
+      <li class="item">Ab, ipsam ratione! Est recusandae, itaque delectus eum cumque velit.</li>
+      <li class="item">Sint quae nulla possimus alias laborum laboriosam sequi dicta vitae.</li>
+      <li class="item">Ratione eius iure excepturi, aperiam tempore id reiciendis porro sit!</li>
+
+      <h1 class="bg-blue-grey-3">hi</h1>
+    </ul>
   </q-page>
 </template>
 
-<script>
-export default {
-}
-</script>
+<script></script>
+<style></style>
